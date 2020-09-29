ca691b77d6406b97bf1f243b4528ef15adc76f71
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-09-29 13:06:32 +1000

    Misc. nvim changes/reorganisation

--- home.nix
+++ home.nix
@@ -47,7 +47,7 @@
       font = {
         size = 10.0;
         normal.family = "RobotoMono Nerd Font";
-      };      
+      };
       colors = {
         primary = {
           background = "#1b1918";
@@ -136,9 +136,9 @@
 
   # Firefox
   programs.firefox = {
-    enable = true; 
+    enable = true;
   };
-  
+
   # Feh
   programs.feh.enable = true;
 
@@ -205,41 +205,34 @@
       vim-nix
     ];
     extraConfig = ''
-      set nocompatible
-      
       " CoC settings
       " Cycle completion options
       inoremap <silent><expr> <TAB>
-            \ pumvisible() ? "\<C-n>" :
-            \ <SID>check_back_space() ? "\<TAB>" :
-            \ coc#refresh()
+        \ pumvisible() ? "\<C-n>" :
+        \ <SID>check_back_space() ? "\<TAB>" :
+        \ coc#refresh()
       inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-      
+
       function! s:check_back_space() abort
         let col = col('.') - 1
         return !col || getline('.')[col - 1]  =~# '\s'
       endfunction
-      
       " Open completion menu
       inoremap <silent><expr> <c-space> coc#refresh()
-      
       " Accept completion option
       inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
-      
-      " Documentation
-      nnoremap <silent> K :call <SID>show_documentation()<CR>
-      
-      function! s:show_documentation()
-        if (index(['vim','help'], &filetype) >= 0)
-          execute 'h '.expand('<cword>')
-        else
-          call CocAction('doHover')
-        endif
-      endfunction
-      
       " Highlight symbol under cursor on CursorHold
       autocmd CursorHold * silent call CocActionAsync('highlight')
-      
+
+      " Fugitive settings
+      nnoremap <silent> <Leader>ga :G add %:r\.*<CR>
+      nnoremap <Leader>gc :G commit -m ""<Left>
+      nnoremap <silent> <Leader>gp :G push<CR>
+      nnoremap <silent> <Leader>gs :G status<CR>
+
+      " Sneak settings
+      let g:sneak#s_next = 1
+
       " Vimtex / UltiSnips settings
       let g:tex_flavor='latex'
       let g:vimtex_view_method='zathura'
@@ -250,100 +243,112 @@
       let g:UltiSnipsExpandTrigger='�'
       let g:UltiSnipsJumpForwardTrigger='<tab>'
       let g:UltiSnipsJumpBackwardTrigger='<s-tab>'
+      " Clear extra files
       autocmd VimLeave *.tex !tex-clear %
+      " Spell checker/corrector
       autocmd FileType tex setlocal spell
       set spelllang=en_gb
       inoremap <C-l> <c-g>u<Esc>[s1z=`]a<c-g>u
+      " Word count
       nnoremap <Leader>lw :VimtexCountWords<CR>
 
-      " Fugitive settings
-      nnoremap <silent> <Leader>ga :G add <C-R>%<BS><BS><BS>*<CR>
-      nnoremap <silent> <Leader>gc :G commit<CR>
-      nnoremap <silent> <Leader>gp :G push<CR>
-      nnoremap <silent> <Leader>gs :G status<CR>
-      
       " Colours
+      " Brackets
+      highlight MatchParen cterm=bold ctermbg=none ctermfg=red
+      " CoC menu
+      highlight Pmenu ctermbg=darkgrey ctermfg=black
+      highlight PmenuSel ctermbg=green ctermfg=black
       " Gitgutter
       highlight GitGutterAdd ctermfg=green
       highlight GitGutterChange ctermfg=yellow
       highlight GitGutterDelete ctermfg=red
-      " CoC menu
-      highlight Pmenu ctermbg=darkgrey ctermfg=white
-      highlight PmenuSel ctermbg=green ctermfg=white
       " Vimtex Conceal
       highlight Conceal ctermbg=none ctermfg=red
-      " Brackets
-      highlight MatchParen cterm=bold ctermbg=none ctermfg=red
-      
+
       " Always show signcolumns
       set signcolumn=yes
       highlight clear SignColumn
-      
+
       " Line numbers
       set number relativenumber
-      
+
       " Keep hidden buffers loaded
       set hidden
-      
+
       " Case-insensitive search, unless contains caps
       set ignorecase
       set smartcase
-      
+
       " Show partial matches
       set incsearch
-      
+
       " Highlight search matches
       set hlsearch
-      
+
       " Clear search highlighting
       nnoremap <silent> <esc> :nohlsearch<CR>
-      
+
       " Indentation
       set autoindent
       set expandtab
       set softtabstop=4
-      
+
       " Height below status bar
       set cmdheight=2
-      
+
       " CursorHold timing
       set updatetime=300
-      
+
       " Don't give |ins-completion-menu| messages
+      set shortmess=F
       set shortmess+=c
-      
+
       " Highlight matching brackets
       set showmatch
       set matchtime=2
-      
+
       " Don't break words to wrap
       set linebreak
-      
+
       " Keep cursor off the edge
       set scrolloff=6
       set sidescrolloff=6
-      
+
       " Popup menu for commands
       set wildmenu
-      
+
+      " Switch behaviour of g on s
+      set gdefault
+
       " Enable filetype features
       filetype indent plugin on
-      
+
       " Backspace over whitespace
       set backspace=indent,eol,start
-      
+
       " Enable syntax highlighting
       syntax on
-      
-      " Clipboard 
+
+      " Clipboard
       set clipboard=unnamedplus
-      
-      set shortmess=F
-      
+
+      " No auto-comment on newline
+      autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+
+      " Sensible split directions
+      set splitbelow splitright
+
+      " I forgot to edit with sudo
+      cnoremap w!! execute 'silent! write !sudo tee % >/dev/null' <bar> edit!
+
+      " Automatically remove trailing whitespace and newlines.
+      autocmd BufWritePre * %s/\s\+$//e
+      autocmd BufWritepre * %s/\n\+\%$//e
+
+      " Misc
+      set nocompatible
       set encoding=utf-8
-      
       set t_Co=256
-      
       set laststatus=2
     '';
   };
@@ -566,15 +571,15 @@
 
         bars = [];
       };
-      # Fix 
+      # Fix
       extraConfig = ''
-        # media controls 
+        # media controls
         bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +1% #increase sound volume
         bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -1% #decrease sound volume
         bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle # mute sound
         bindsym XF86AudioPlay exec playerctl play-pause
         bindsym XF86AudioStop exec playerctl stop
-        bindsym XF86AudioNext exec playerctl next 
+        bindsym XF86AudioNext exec playerctl next
         bindsym XF86AudioPrev exec playerctl previous
       '';
     };
