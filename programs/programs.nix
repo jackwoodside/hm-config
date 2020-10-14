378776a298b94a6b7f04716cbc85cb940e0dd65d
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-10-14 18:19:35 +1100

    Module stuff

--- programs/programs.nix
+++ programs/programs.nix
@@ -1,1 +1,42 @@
 { config, pkgs, ... };
+
+{
+  # Enable font management.
+  fonts.fontconfig.enable = true;
+
+  # Misc packages.
+  home.packages = with pkgs; [
+    # Fonts
+    roboto-mono
+    font-awesome
+    (nerdfonts.override {
+      fonts = [ "RobotoMono" ];
+    })
+
+    # Programs
+    acpilight # Laptop
+    arandr # Laptop, temporary
+    bitwarden-cli
+    discord
+    exa
+    fd
+    gimp
+    libnotify
+    maim
+    mathematica # Laptop
+    networkmanager_dmenu # Laptop
+    pavucontrol # Laptop, temporary
+    playerctl
+    python3Minimal
+    ranger
+    ripgrep
+    spotify
+    steam
+    sxiv
+    teams # Laptop
+    ueberzug
+    unzip
+    xclip
+    zoom-us # Laptop
+  ];
+};
