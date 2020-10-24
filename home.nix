5ccb1d13b45b339bb404db2b9ed7297576370637
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-10-24 23:55:59 +1100

    Big i3 overhaul

--- home.nix
+++ home.nix
@@ -41,8 +41,8 @@
     maim
     #mathematica # Laptop
     #networkmanager_dmenu # Laptop
-    #pavucontrol # Laptop, temporary
     playerctl
+    pulsemixer
     python3Minimal
     ranger
     ripgrep
@@ -675,6 +675,118 @@
         };
 
         bars = [];
+
+        keybindings = {
+          let
+            modifier = config.xsession.windowManager.i3.config.modifier;
+          in lib.mkOptionDefault {
+            # toggle split direction
+            "${modifier}+t" = "split toggle";
+            # toggle screen that current workspace is on
+            "${modifier}+x" = "move workspace to output right";
+            # audio profile
+            "${modifier}+a" = "exec --no-startup-id pactl set-card-profile 0 'output:analog-stereo'";
+            "${modifier}+Shift+a" = "exec --no-startup-id pactl set-card-profile 0 'output:hdmi-stereo'";
+            # backlight
+            "XF86MonBrightnessUp" = "exec xbacklight -inc 5";
+            "XF86MonBrightnessDown" = "exec xbacklight -dec 5";
+            # display manager
+            "${modifier}+p" = "exec display-menu";
+            # emoji menu
+            "${modifier}+u" = "exec unicode-menu";
+            # media controls
+            "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +1%";
+            "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -1%";
+            "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+            "XF86AudioPlay" = "exec playerctl play-pause";
+            "XF86AudioStop" = "exec playerctl stop";
+            "XF86AudioNext" = "exec playerctl next ";
+            "XF86AudioPrev" = "exec playerctl previous";
+            # mounting
+            "${modifier}+m" = "exec --no-startup-id mount-menu";
+            "${modifier}+Shift+m" = "exec --no-startup-id umount-menu";
+            # network manager
+            "${modifier}+n" = "exec networkmanager_dmenu";
+            # password manager
+            "${modifier}+z" = "exec bwmenu";
+            # ranger
+            "${modifier}+w" = "exec alacritty -e ranger";
+            # system menu
+            "${modifier}+BackSpace" = "exec system-menu";
+            # screenshots
+            "Shift+Print" = "exec maim -u -s ~/Pictures/$(date +%F-%s).png";
+            "Print" = "exec maim -u -s | xclip -selection clipboard -t image/png";
+            "${modifier}+Print" = "exec maim -u ~/Pictures/$(date +%F-%s).png";
+            # task manager
+            "Control+Mod1+Delete" = "exec --no-startup-id alacritty --class taskmanager -e htop";
+          };
+        };
+
+        startup = [
+          { command = "systemctl --user restart polybar"; always = true; notification = false; }
+        ];
+
+        extraConfig = "
+          # dropdown terminal
+          bindsym $mod+Delete [instance="dropdown"] scratchpad show; [instance="dropdown"] move position center
+          for_window [instance="dropdown"] floating enable
+          for_window [instance="dropdown"] move scratchpad
+          for_window [instance="dropdown"] resize set 1200 700
+          exec --no-startup-id alacritty --class dropdown
+          bindsym $mod+Shift+Delete exec --no-startup-id alacritty --class dropdown
+
+          # picture-in-picture
+          for_window [title="(?i)picture-in-picture"] sticky enable
+          for_window [title="(?i)picture-in-picture"] move scratchpad, scratchpad show
+          bindsym $mod+v [title="(?i)picture-in-picture"] scratchpad show
+
+          # task manager
+          for_window [instance="taskmanager"] floating enable
+          for_window [instance="taskmanager"] resize set 1200 700
+          for_window [instance="taskmanager"] move position center
+
+          # worksapce settings
+          set $workspace1 "1:"
+          set $workspace2 "2:"
+          set $workspace3 "3:"
+          set $workspace4 "4:"
+          set $workspace5 "5:"
+          set $workspace6 "6:"
+          set $workspace7 "7:"
+          set $workspace8 "8:"
+          set $workspace9 "9:"
+          set $workspace10 "10:"
+
+          bindsym $mod+1 workspace $workspace1
+          bindsym $mod+2 workspace $workspace2
+          bindsym $mod+3 workspace $workspace3
+          bindsym $mod+4 workspace $workspace4
+          bindsym $mod+5 workspace $workspace5
+          bindsym $mod+6 workspace $workspace6
+          bindsym $mod+7 workspace $workspace7
+          bindsym $mod+8 workspace $workspace8
+          bindsym $mod+9 workspace $workspace9
+          bindsym $mod+0 workspace $workspace10
+
+          bindsym $mod+Shift+1 move container to workspace $workspace1
+          bindsym $mod+Shift+2 move container to workspace $workspace2
+          bindsym $mod+Shift+3 move container to workspace $workspace3
+          bindsym $mod+Shift+4 move container to workspace $workspace4
+          bindsym $mod+Shift+5 move container to workspace $workspace5
+          bindsym $mod+Shift+6 move container to workspace $workspace6
+          bindsym $mod+Shift+7 move container to workspace $workspace7
+          bindsym $mod+Shift+8 move container to workspace $workspace8
+          bindsym $mod+Shift+9 move container to workspace $workspace9
+          bindsym $mod+Shift+0 move container to workspace $workspace10
+
+          assign [class="(?i)firefox"] $workspace1
+          assign [class="discord"] $workspace2
+          assign [class="(?i)steam"] $workspace5
+          assign [class="itch"] $workspace5
+          assign [class="(?i)zathura"] $workspace3
+          assign [class="(?i)mathematica"] $workspace3
+          for_window [class="Spotify"] move to workspace $workspace10
+        ";
       };
     };
   };
