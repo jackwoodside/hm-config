92be9a5694de8148891470844ecfc2a99b641512
0003c9fe <20267593+0003c9fe@users.noreply.github.com>
2020-12-13 20:12:15 +1100

    Updated htop config

--- programs/htop.nix
+++ programs/htop.nix
@@ -3,6 +3,20 @@
 {
   programs.htop = {
     enable = true;
+    enableMouse = false;
+    fields = [
+      "PERCENT_CPU" "PERCENT_MEM" "STATE" "COMM"
+    ];
+    meters = {
+      left = [
+        "LeftCPUs" "Memory"
+      ];
+      right = [
+        "RightCPUs" "Uptime"
+      ];
+    };
+    showProgramPath = false;
+    sortKey = "PERCENT_MEM";
     treeView = true;
   };
 }
