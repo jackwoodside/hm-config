b1e6ed89d0ae9cba837acf9cc9b3634fecf5184e
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2021-10-04 23:50:37 +1100

    Attempt to fix htop

--- programs/htop.nix
+++ programs/htop.nix
@@ -5,15 +5,15 @@
     enable = true;
     settings = {
       enable_mouse = false;
-      fields = [
-        "PERCENT_CPU" "PERCENT_MEM" "STATE" "COMM"
-      ];
-      left_meters = [
-        "LeftCPUs" "Memory"
-      ];
-      right_meters = [
-        "RightCPUs" "Uptime"
-      ];
+    #  fields = [
+    #    "PERCENT_CPU" "PERCENT_MEM" "STATE" "COMM"
+    #  ];
+    #  left_meters = [
+    #    "LeftCPUs" "Memory"
+    #  ];
+    #  right_meters = [
+    #    "RightCPUs" "Uptime"
+    #  ];
       show_program_path = false;
       sort_key = "PERCENT_MEM";
       tree_view = true;
