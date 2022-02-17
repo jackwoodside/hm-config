ba9c0364fd44d925faca274843e02910a93f2b76
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-02-17 16:43:52 +1100

    Fixed redundant entry in laptop profile

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -11,10 +11,4 @@
     normal.family = "RobotoMono Nerd Font";
     size = 8.0;
   };
-
-  # Network Manager
-  xdg.configFile."networkmanager-dmenu/config.ini".text = ''
-    [dmenu]
-    dmenu_command = rofi
-  '';
 }
