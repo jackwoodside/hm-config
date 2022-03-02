3f741e2c42992274f62e2caa3636098edd75b7ef
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-03-02 14:08:36 +1100

    Changes to scripts

--- programs/scripts/default.nix
+++ programs/scripts/default.nix
@@ -3,19 +3,14 @@
 {
   xdg.dataFile = {
     "emoji".source = ./emoji;
-    "screenlayouts/tv".source = ./screenlayouts/tv;
-    "screenlayouts/laptop".source = ./screenlayouts/laptop;
     "screenlayouts/desktop".source = ./screenlayouts/desktop;
   };
 
   home.file = {
-    ".local/bin/display-menu".source = ./display-menu;
     ".local/bin/extract".source = ./extract;
-    ".local/bin/lock".source = ./lock;
-    ".local/bin/mount-menu".source = ./mount-menu;
-    ".local/bin/system-menu".source = ./system-menu;
+    ".local/bin/rofi-mount".source = ./rofi-mount;
     ".local/bin/tex-clear".source = ./tex-clear;
-    ".local/bin/umount-menu".source = ./umount-menu;
-    ".local/bin/unicode-menu".source = ./unicode-menu;
+    ".local/bin/rofi-umount".source = ./rofi-umount;
+    ".local/bin/rofi-emoji".source = ./rofi-emoji;
   };
 }
