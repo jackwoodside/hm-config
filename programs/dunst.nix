c08b1e76f1fa47d562f52c094166788a3cc0e326
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-03-02 14:09:23 +1100

    Updated dunst to new syntax

--- programs/dunst.nix
+++ programs/dunst.nix
@@ -5,40 +5,29 @@
     enable = true;
     settings = {
       global = {
-        geometry = "350x5-10+10";
-        transparency = 20;
-        notification_height = 0;
-        separator_height = 2;
-        padding = 0;
-        horizontal_padding = 8;
-        frame_width = 2;
+        offset = "10x10";
+        transparency = "20";
+        frame_width = "2";
         separator_color = "frame";
-        sort = "yes";
-        idle_threshold = 120;
         font = "RobotoMono 16";
+        idle_threshold = 120;
         line_height = 0;
-        markup = "full";
-        format = "<b>%s</b>\n%b";
-        alignment = "left";
         word_wrap = "yes";
-        ignore_newline = "no";
-        stack_duplicates = true;
-        show_indicators = "no";
-      };
-      shortcuts = {
-        close = "ctrl+space";
-        close_all = "ctrl+shift+space";
-        history = "ctrl+grave";
-        context = "ctrl+shift+period";
+        show_indicators = "false";
+
+        mouse_left_click = "none";
+        mouse_right_click = "none";
       };
       urgency_normal = {
         background = "#181818";
         foreground = "#dedede";
+        frame_color = "#dedede";
         timeout = 5;
       };
       urgency_low = {
         background = "#181818";
         foreground = "#dedede";
+        frame_color = "#dedede";
         timeout = 5;
       };
       urgency_critical = {
