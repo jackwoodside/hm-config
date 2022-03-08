4a7f60311c2b68db48dc0d69725f62231edb256a
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-03-08 21:49:36 +1100

    Work on htop

--- programs/htop.nix
+++ programs/htop.nix
@@ -1,19 +1,25 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.htop = {
     enable = true;
     settings = {
+      cpu_count_from_one = 1;
       enable_mouse = false;
-    #  fields = [
-    #    "PERCENT_CPU" "PERCENT_MEM" "STATE" "COMM"
-    #  ];
-    #  left_meters = [
-    #    "LeftCPUs" "Memory"
-    #  ];
-    #  right_meters = [
-    #    "RightCPUs" "Uptime"
-    #  ];
+      fields = with config.lib.htop.fields; [
+        PERCENT_CPU
+        PERCENT_MEM
+        STATE
+        COMM
+      ];
+      left_meters = with config.lib.htop; [
+        "LeftCPUs"
+        "Memory"
+      ];
+      right_meters = with config.lib.htop; [
+        "RightCPUs"
+        "Uptime"
+      ];
       show_program_path = false;
       sort_key = "PERCENT_MEM";
       tree_view = true;
