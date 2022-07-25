391e47383df3c9e16623f8ed643cf31ae344e308
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-07-25 16:56:25 +1000

    Added backend for picom to stop screentearing on nvidia

--- programs/picom.nix
+++ programs/picom.nix
@@ -3,9 +3,10 @@
 {
   services.picom = {
     enable = true;
+    backend = "glx";
     fade = true;
     fadeDelta = 4;
-    fadeSteps = [ "0.03" "0.03" ];
+    fadeSteps = [ 0.03 0.03 ];
     vSync = true;
   };
 }
