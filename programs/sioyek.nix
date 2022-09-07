f8b3737245f038bf263f527e7479d7476a816709
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-09-08 07:49:45 +1000

    Added google keybinding for sioyek

--- programs/sioyek.nix
+++ programs/sioyek.nix
@@ -8,7 +8,7 @@
       "move_down" = "j";
       "move_left" = "h";
       "move_right" = "l";
-      "fit_to_page_width" = "s";
+      "fit_to_page_width" = "f";
       "quit" = "q";
     };
     config = {
@@ -16,6 +16,9 @@
       "touchpad_sensitivity" = "0.4";
       "page_separator_width" = "1";
       "page_separator_color" = "0.09 0.09 0.09";
+
+      "search_url_g" = "https://www.google.com/search?q=";
+      "should_launch_new_instance" = "1";
     };
   };
 }
