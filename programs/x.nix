e9399ed5b96ed30e0bb98fc3725a29a202c495d4
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-10-10 21:49:53 +1100

    Updated to new name for gnome

--- programs/x.nix
+++ programs/x.nix
@@ -12,7 +12,7 @@
   };
   home.pointerCursor = {
     x11.enable = true;
-    package = pkgs.gnome3.adwaita-icon-theme;
+    package = pkgs.gnome.adwaita-icon-theme;
     name = "Adwaita";
     size = 16;
   };
