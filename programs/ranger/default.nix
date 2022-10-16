0e7cdddc2363af73771ecb8e9a01f425fa65bf8a
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-10-16 17:01:15 +1100

    Added ranger_devicons plugin

--- programs/ranger/default.nix
+++ programs/ranger/default.nix
@@ -6,5 +6,8 @@
     "ranger/rc.conf".source = ./rc.conf;
     "ranger/rifle.conf".source = ./rifle.conf;
     "ranger/scope.sh".source = ./scope.sh;
+    # devicons plugin from https://github.com/alexanderjeurissen/ranger_devicons
+    "ranger/plugins/ranger_devicons/__init__.py".source = ./__init__.py;
+    "ranger/plugins/ranger_devicons/devicons.py".source = ./devicons.py;
   };
 }
