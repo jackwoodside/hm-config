57c58ac39f66c276316705003a3df8c2a48c4231
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-10-16 16:50:11 +1100

    Added zsh git aliases

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -30,8 +30,10 @@
       c = "clear";
       cdc = "cd && clear";
       pd = "mpv --really-quiet $PWD & disown && exit";
-      gs = "git status -uno";
-      gsa = "git status";
+      gs = "git status";
+      gp = "git push";
+      go = "git pull";
+      gd = "git diff";
       server = "ssh -Y -L 5901:127.0.0.1:5962 jaw103@nucl03";
       vnc = "vncviewer :1";
       mountserver = "sshfs -o reconnect,ServerAliveInterval=20,idmap=user jaw103@nucl03:/scratch3/labr ~/Documents/server";
