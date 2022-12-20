8d246ae82032a7f71a0fa50b4e3ae598c1e50db6
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2022-12-20 18:26:14 +1100

    Removed Packer autosync, doesn't work on nix setups

--- programs/neovim/lua/plugins.lua
+++ programs/neovim/lua/plugins.lua
@@ -1,10 +1,3 @@
--- Automatically compile packer plugins whenever this file is updated
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
-	pattern = "plugins.lua",
-	command = "source <afile> | PackerCompile",
-})
-
 return require("packer").startup({
 	function(use)
 		-- Package manager
