69dc49646859319a09e34feb92f976edfaebbef8
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-01-10 23:31:19 +1100

    Updated neovim config

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -5,12 +5,14 @@
     enable = true;
     vimAlias = true;
     withNodeJs = true;
+    withPython3 = true;
   };
 
-  home.packages = with pkgs; [ gcc ];
+  home.packages = with pkgs; [ cargo gcc gnumake ];
   xdg.configFile."nvim".source = builtins.fetchGit {
       url = "git@github.com:jackwoodside/nvim-config.git";
-      ref = "main";
       shallow = true;
+      ref = "main";
+      rev = "58cbb61acc3be673a4162e10bf9eccc55d10b679";
   };
 }
