e827dcbc2ca91833d38dc5cec8cf98e2cea9dcd8
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-01-11 16:36:32 +1100

    Updated neovim config

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,18 +1,31 @@
 { pkgs, ... }:
 
 {
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    withNodeJs = true;
-    withPython3 = true;
-  };
+    programs.neovim = {
+        enable = true;
+        vimAlias = true;
+        withNodeJs = true;
+        withPython3 = true;
+# Required for LSPs, mason doesn't always work on nix
+        extraPackages = with pkgs; [
+            clang-tools
+            fortran-language-server
+            ltex-ls
+            nil
+            python310Packages.jedi-language-server
+            python310Packages.autopep8
+            shfmt
+            stylua
+            sumneko-lua-language-server
+            texlab
+        ];
+    };
 
-  home.packages = with pkgs; [ cargo gcc gnumake ];
-  xdg.configFile."nvim".source = builtins.fetchGit {
-      url = "git@github.com:jackwoodside/nvim-config.git";
-      shallow = true;
-      ref = "main";
-      rev = "18834986e53337ce946e8a30a01579d83ba9dc04";
-  };
+    home.packages = with pkgs; [ cargo gcc gnumake ];
+    xdg.configFile."nvim".source = builtins.fetchGit {
+        url = "git@github.com:jackwoodside/nvim-config.git";
+        shallow = true;
+        ref = "main";
+        rev = "fe4656d8932c46fd2912cb7d54b7793bd73b78cf";
+    };
 }
