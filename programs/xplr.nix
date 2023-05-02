f4d9c2d56f920ea1bfcee00c1c7fb0b5f36cbdbb
jackwoodside <20267593+0003c9fe@users.noreply.github.com>
2023-05-02 14:10:06 +1000

    Merge

---
+++ programs/xplr.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+    xdg.configFile."xplr".source = builtins.fetchGit {
+        url = "git@github.com:jackwoodside/xplr-config.git";
+        shallow = true;
+        ref = "main";
+        rev = "";
+    };
+}
